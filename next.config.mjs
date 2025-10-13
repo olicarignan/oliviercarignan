4de3ab08a6c7669669d3a47206bb23105603f337
Olivier Carignan <olive.carignan@gmail.com>
2025-10-12 20:07:06 -0400

    compress cache export

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   transpilePackages: ["gsap"],
   reactStrictMode: true,
+  webpack: (config) => {
+    config.cache.compression = "brotli";
+    return config;
+  },
 };
 
 export default nextConfig;
