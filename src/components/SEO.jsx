c48df67781945b3a0ab44c1f54272cad5509cd3d
Olivier Carignan <olive.carignan@gmail.com>
2026-04-08 10:41:16 -0400

    seo settings

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -4,6 +4,7 @@ export default function SEO() {
   const title = "Olivier Carignan";
   const description = "Designer, Engineer & Founder";
   const url = "https://oliviercarignan.com";
+  const image = `${url}/images/png/og_image.png`;
 
   return (
     <Head>
@@ -11,19 +12,24 @@ export default function SEO() {
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="canonical" href={url} />
 
       {/* Open Graph */}
       <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:locale" content="en_US" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={url} />
+      <meta name="twitter:image" content={image} />
     </Head>
   );
 }
